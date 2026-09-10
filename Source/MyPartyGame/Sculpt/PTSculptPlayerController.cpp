3bd581db08e0f9191ebb39948fe0e8b6241bd76a
FrancoAgnic <franco.agnic@hotmail.com>
2026-09-09 23:05:43 -1000

    Revert "Personajes: brazos de cable con fisica + esfera-mano (CableComponent); brazo del hombro al preview del escultor del turno (usa ReplBrush.Pos) para que se vea quien maneja el sello. Plugin CableComponent habilitado"
    This reverts commit 5bd984cb9f61842a0fdc8cf3071e1c739156d504.

--- Source/MyPartyGame/Sculpt/PTSculptPlayerController.cpp
+++ Source/MyPartyGame/Sculpt/PTSculptPlayerController.cpp
@@ -785,7 +785,6 @@ void APTSculptPlayerController::PlayerTick(float DeltaTime)
             B.bEyes   = bEyesTool;
             B.bActive = true;
             MyPawn->Server_ReportBrush(B);
-            MyPawn->SetBrushLocal(B); // COND_SkipOwner no le devuelve el suyo → setearlo para su propio brazo→preview
         }
     }
 
